96a9071ec52fe6f6923d66e99a68fc47a82572df
Marina <marina.zilkina@gmail.com>
2023-07-03 23:39:22 +0300

    problem solved

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import Button from '@mui/material/Button';
-import {Grid, Typography} from "@mui/material";
-
-//import { Button, Grid, Typography, TextField, FormHelperText, FormControl, FormControlLabel, RadioGroup, Radio } from "@mui/material";
-
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 export default class CreateRoomPage extends Component{
     defaultVotes =2;
     constructor(props) {
@@ -11,11 +16,35 @@ export default class CreateRoomPage extends Component{
     }
 
     render() {
-        return <Grid container spacing={1}>
-                    <Grid item xs={12} align="center"></Grid>
-                        <Typography component="h4" variant="h4">
-                            Create a Room
-                        </Typography>
-                </Grid>;
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography component="h4" variant="h4">
+                        Create a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl component="fieldset">
+                        <FormHelperText>
+                            <div align='center'>
+                                Guest Content of Playback State
+                            </div>
+                        </FormHelperText>
+                        <RadioGroup row defaultValue='true'>
+                            <FormControlLabel
+                                value="true"
+                                control={<Radio color="primary"/>}
+                                label="Play/Pause"
+                                labelPlacement="bottom"/>
+                            <FormControlLabel
+                                value="false"
+                                control={<Radio color="secondary"/>}
+                                label="No Control"
+                                labelPlacement="bottom"/>
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+            </Grid>
+        );
     }
 }
